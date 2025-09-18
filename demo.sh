23ee2e349ece5e240259fb6abde4bc3a34844b9f
Herroa <roman.gordov@mail.ru>
2025-09-18 19:02:08 +0700

    init

---
+++ demo.sh
@@ -0,0 +1,11 @@
+# docker exec -it demo-postgres psql -U demo_user -d demo
+# docker-compose up -d
+
+docker cp demo-big-20170815.sql demo-postgres:demo-big-20170815.sql
+docker exec -it demo-postgres psql -U demo_user -d demo -f ./demo-big-20170815.sql 
+
+docker exec -it my-postgres sh
+
+psql -U postgre
+
+docker exec -i my-postgres psql -U postgres -d  postgres -f /docker-entrypoint-initdb.d/demo-big-20170815.sql
