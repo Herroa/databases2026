7b43f9e3f8b80a9e507c954bb5e72c7ef654f753
Herroa <roman.gordov@mail.ru>
2025-09-18 19:44:32 +0700

    add instruction and sql script, remove copy file

--- demo.sh
+++ demo.sh
@@ -1,11 +1,27 @@
-# docker exec -it demo-postgres psql -U demo_user -d demo
 # docker-compose up -d
 
-docker cp demo-big-20170815.sql demo-postgres:demo-big-20170815.sql
-docker exec -it demo-postgres psql -U demo_user -d demo -f ./demo-big-20170815.sql 
-
-docker exec -it my-postgres sh
-
-psql -U postgre
 
 docker exec -i my-postgres psql -U postgres -d  postgres -f /docker-entrypoint-initdb.d/demo-big-20170815.sql
+
+-U username
+-d database
+
+psql commands
+\l                          -- список БД
+\c mydb                     -- переключиться на mydb
+\dt                         -- список таблиц
+\d users                    -- структура таблицы users
+SELECT * FROM users LIMIT 5;
+\x                          -- включить вертикальный вывод
+\df                         -- список функций
+\du                         -- список ролей
+\conninfo                   -- текущее подключение
+\q                          -- выход
+
+CREATE DATABASE sports_club ENCODING 'UTF8' LC_COLLATE 'en_US.UTF-8' LC_CTYPE 'en_US.UTF-8' TEMPLATE template0;
+\c sports_club
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+apt-get update && apt-get install -y nano fish
+
+psql -U postgres -d sports_club -f ~/init_db.sql
