0b04dad38d95b68a5f81f784ee08d5bd026f6ed3
Ivan <ivan.mikhalov.1104@gmail.com>
2025-09-18 21:35:35 +0700

    modif 'generate_3m_bookings.sql'

--- generate_3m_bookings.sql
+++ generate_3m_bookings.sql
@@ -1,1 +1,7 @@
--- to do script
+INSERT INTO attendance_logs (user_id, schedule_id, checked_in, checked_out)
+SELECT
+    (random() * 1000)::INT + 1,
+    (random() * 500)::INT + 1,
+    NOW() - (random() * interval '365 days'),
+    NOW() - (random() * interval '365 days')
+FROM generate_series(1, 3000000);
